9673d6bf39d642d2c476fa9817789e8c2d8fa2e1
hsz <249808574@qq.com>
2019-11-08 11:29:00 +0800

    delete

--- lib/xt_home.dart
+++ lib/xt_home.dart
@@ -6,18 +6,6 @@ class XTHome extends StatefulWidget {
 }
 
 class XTHomeState extends State<XTHome> with AutomaticKeepAliveClientMixin {
-  final topBar = AppBar(
-    backgroundColor: Color(0xffff0000),
-    centerTitle: true,
-    elevation: 1.0,
-    leading: Icon(Icons.camera_alt),
-    title: Center(child: Icon(Icons.search),),
-    actions: <Widget>[
-      Padding(
-        padding: const EdgeInsets.only(right: 12.0),
-        child: Icon(Icons.send),),
-    ],
-  );
 
   int count = 0;
   void addCount() {
@@ -36,7 +24,7 @@ class XTHomeState extends State<XTHome> with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: topBar,
+      appBar: null,
       backgroundColor: Colors.red,
       body: Center(
           child: Text('First: $count', style: TextStyle(fontSize: 30))
